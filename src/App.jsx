1bcc3e66913abe56b7fb12dae2c3006d8bfd35a0
ShivaniRotliwala <shivanirotliwala79@gmail.com>
2024-04-11 18:10:21 +0530

    Footer and book now section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import "./App.scss";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Body from "./components/Body";
 import Home from "./pages/Home";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Body />
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
       <Home />
       <Footer />
     </>
