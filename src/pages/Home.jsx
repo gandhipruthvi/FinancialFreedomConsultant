e104ec8b4d27d6a62bcbde109a61b8b59c418cd1
gandhipruthvi <gandhi.pruthvi@gmail.com>
2024-04-13 16:43:54 +0530

    small changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -21,8 +21,8 @@ function Home() {
       <section className="bookNowSection">
         <div className="body-text-content">
           <p className="body-description">
-            Welcome to <b>Financial Freedom Consultant</b>, where our commitment lies
-            in assisting clients in attaining their financial aspirations.
+            Welcome to <b>Financial Freedom Consultant</b>, where our commitment
+            lies in assisting clients in attaining their financial aspirations.
           </p>
         </div>
 
@@ -43,99 +43,139 @@ function Home() {
       <section className="serviceSection">
         <div className="marqueeText runRight stokeText">
           <h2>Financial Freedom Consultant.</h2>
-        </div><br />
-        <div class="container largeContainer">
-          <div class="row">
-            <div class="col-xl-6">
-              <div class="boxService">
-                <h2 class="secTitle">We Make Your Agency <span>Full Bright</span></h2>
-                <p>This Cheat Sheet offers tips and advice to help you get the essentials right. here’s a list
-                  of the most important tasks of business accounting and their meaning.</p>
+        </div>
+        <br />
+        <div className="container largeContainer">
+          <div className="row">
+            <div className="col-xl-6">
+              <div className="boxService">
+                <h2 className="secTitle">
+                  We Make Your Agency <span>Full Bright</span>
+                </h2>
+                <p>
+                  This Cheat Sheet offers tips and advice to help you get the
+                  essentials right. here’s a list of the most important tasks of
+                  business accounting and their meaning.
+                </p>
               </div>
             </div>
-            <div class="col-xl-3 col-md-6">
-              <div class="icon_box_01">
-                <div class="ibMeta">
+            <div className="col-xl-3 col-md-6">
+              <div className="icon_box_01">
+                <div className="ibMeta">
                   <span>01</span>
-                  <i class="icons-monitoring"></i>
-                  <div class="brleft"></div>
-                  <div class="brright"></div>
+                  <i className="icons-monitoring"></i>
+                  <div className="brleft"></div>
+                  <div className="brright"></div>
                 </div>
-                <h3><a href="single-service.html">Market Research</a></h3>
-                <p>A typical business holds many different assets called capital, including office...</p>
-                {/* <a class="sm" href="single-service.html">Read More<i class="twi-arrow-right1"></i></a> */}
+                <h3>
+                  <a href="single-service.html">Market Research</a>
+                </h3>
+                <p>
+                  A typical business holds many different assets called capital,
+                  including office...
+                </p>
+                {/* <a className="sm" href="single-service.html">Read More<i className="twi-arrow-right1"></i></a> */}
               </div>
             </div>
-            <div class="col-xl-3 col-md-6">
-              <div class="icon_box_01">
-                <div class="ibMeta">
+            <div className="col-xl-3 col-md-6">
+              <div className="icon_box_01">
+                <div className="ibMeta">
                   <span>02</span>
-                  <i class="icon-local_3-1"></i>
-                  <div class="brleft"></div>
-                  <div class="brright"></div>
+                  <i className="icon-local_3-1"></i>
+                  <div className="brleft"></div>
+                  <div className="brright"></div>
                 </div>
-                <h3><a href="single-service.html">Startup Business</a></h3>
-                <p>A typical business holds many different assets called capital, including office...</p>
-                {/* <a class="sm" href="single-service.html">Read More<i class="twi-arrow-right1"></i></a> */}
+                <h3>
+                  <a href="single-service.html">Startup Business</a>
+                </h3>
+                <p>
+                  A typical business holds many different assets called capital,
+                  including office...
+                </p>
+                {/* <a className="sm" href="single-service.html">Read More<i className="twi-arrow-right1"></i></a> */}
               </div>
             </div>
-            <div class="col-xl-3 col-md-6">
-              <div class="icon_box_01">
-                <div class="ibMeta">
+            <div className="col-xl-3 col-md-6">
+              <div className="icon_box_01">
+                <div className="ibMeta">
                   <span>03</span>
-                  <i class="icon-local_1"></i>
-                  <div class="brleft"></div>
-                  <div class="brright"></div>
+                  <i className="icon-local_1"></i>
+                  <div className="brleft"></div>
+                  <div className="brright"></div>
                 </div>
-                <h3><a href="single-service.html">Business Growth</a></h3>
-                <p>A typical business holds many different assets called capital, including office...</p>
-                {/* <a class="sm" href="single-service.html">Read More<i class="twi-arrow-right1"></i></a> */}
+                <h3>
+                  <a href="single-service.html">Business Growth</a>
+                </h3>
+                <p>
+                  A typical business holds many different assets called capital,
+                  including office...
+                </p>
+                {/* <a className="sm" href="single-service.html">Read More<i className="twi-arrow-right1"></i></a> */}
               </div>
             </div>
-            <div class="col-xl-3 col-md-6">
-              <div class="icon_box_01">
-                <div class="ibMeta">
+            <div className="col-xl-3 col-md-6">
+              <div className="icon_box_01">
+                <div className="ibMeta">
                   <span>04</span>
-                  <i class="icon-local_11"></i>
-                  <div class="brleft"></div>
-                  <div class="brright"></div>
+                  <i className="icon-local_11"></i>
+                  <div className="brleft"></div>
+                  <div className="brright"></div>
                 </div>
-                <h3><a href="single-service.html">Insurance Task</a></h3>
-                <p>A typical business holds many different assets called capital, including office...</p>
-                {/* <a class="sm" href="single-service.html">Read More<i class="twi-arrow-right1"></i></a> */}
+                <h3>
+                  <a href="single-service.html">Insurance Task</a>
+                </h3>
+                <p>
+                  A typical business holds many different assets called capital,
+                  including office...
+                </p>
+                {/* <a className="sm" href="single-service.html">Read More<i className="twi-arrow-right1"></i></a> */}
               </div>
             </div>
-            <div class="col-xl-3 col-md-6">
-              <div class="icon_box_01">
-                <div class="ibMeta">
+            <div className="col-xl-3 col-md-6">
+              <div className="icon_box_01">
+                <div className="ibMeta">
                   <span>05</span>
-                  <i class="icon-Wemseu01"></i>
-                  <div class="brleft"></div>
-                  <div class="brright"></div>
+                  <i className="icon-Wemseu01"></i>
+                  <div className="brleft"></div>
+                  <div className="brright"></div>
                 </div>
-                <h3><a href="single-service.html">Consultancy</a></h3>
-                <p>A typical business holds many different assets called capital, including office...</p>
-                {/* <a class="sm" href="single-service.html">Read More<i class="twi-arrow-right1"></i></a> */}
+                <h3>
+                  <a href="single-service.html">Consultancy</a>
+                </h3>
+                <p>
+                  A typical business holds many different assets called capital,
+                  including office...
+                </p>
+                {/* <a className="sm" href="single-service.html">Read More<i className="twi-arrow-right1"></i></a> */}
               </div>
             </div>
-            <div class="col-xl-3 col-md-6">
-              <div class="icon_box_01">
-                <div class="ibMeta">
+            <div className="col-xl-3 col-md-6">
+              <div className="icon_box_01">
+                <div className="ibMeta">
                   <span>06</span>
-                  <i class="icon-local_1-1"></i>
-                  <div class="brleft"></div>
-                  <div class="brright"></div>
+                  <i className="icon-local_1-1"></i>
+                  <div className="brleft"></div>
+                  <div className="brright"></div>
                 </div>
-                <h3><a href="single-service.html">Financial Advices</a></h3>
-                <p>A typical business holds many different assets called capital, including office...</p>
-                {/* <a class="sm" href="single-service.html">Read More<i class="twi-arrow-right1"></i></a> */}
+                <h3>
+                  <a href="single-service.html">Financial Advices</a>
+                </h3>
+                <p>
+                  A typical business holds many different assets called capital,
+                  including office...
+                </p>
+                {/* <a className="sm" href="single-service.html">Read More<i className="twi-arrow-right1"></i></a> */}
               </div>
             </div>
           </div>
-          <div class="row">
-            <div class="col-lg-12 text-center">
-              <p class="qu_link">You can also find our <a href="contact.html">Consultant Service to contact for
-                the consulting</a></p>
+          <div className="row">
+            <div className="col-lg-12 text-center">
+              <p className="qu_link">
+                You can also find our{" "}
+                <a href="contact.html">
+                  Consultant Service to contact for the consulting
+                </a>
+              </p>
             </div>
           </div>
         </div>
@@ -214,6 +254,8 @@ function Home() {
           <h2>Financial Freedom Consultant.</h2>
         </div>
       </section>
+
+      <section className="whyChooseUs"></section>
     </div>
   );
 }
