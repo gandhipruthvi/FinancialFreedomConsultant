b650d14709e403a16b476f69e2d72aad4ae05f1d
Sakhi Rotliwala <rotliwalasakhi@gmail.com>
2024-06-30 12:52:17 +0530

    added: schedule management calendar

--- src/pages/admin/ScheduleManagement.jsx
+++ src/pages/admin/ScheduleManagement.jsx
@@ -1,10 +1,59 @@
+import DatePicker from "react-datepicker";
 import "../../styles/ScheduleManagement.scss";
+import { useState } from "react";
+import getDay from "date-fns/getDay";
 
 const ScheduleManagement = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [selectedTime, setSelectedTime] = useState(null);
+
+  const isWeekday = (date) => {
+    const day = getDay(date);
+    return day !== 0 && day !== 7;
+  };
+
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+  };
+
+  const handleTimeChange = (time) => {
+    setSelectedTime(time);
+  };
+
   return (
     <div className="scheduleManagementPage">
       <div className="container">
-        <h1>Schedule Management</h1>
+        <DatePicker
+          selected={selectedDate}
+          className={
+            selectedDate == null
+              ? "dateTimeInvalid dateTimeSelector"
+              : "dateTimeSelector"
+          }
+          onChange={handleDateChange}
+          filterDate={isWeekday}
+          minDate={new Date()}
+          dateFormat="dd/MM/yyyy"
+          placeholderText="Select Date"
+          inline
+        />
+        {selectedDate && (
+          <>
+            <DatePicker
+              selected={selectedTime}
+              onChange={handleTimeChange}
+              showTimeSelect
+              showTimeSelectOnly
+              timeIntervals={60}
+              timeCaption="Time"
+              dateFormat="h:mm aa"
+              placeholderText="Select a time"
+              inline
+              minTime={new Date(selectedDate).setHours(9, 0, 0, 0)}
+              maxTime={new Date(selectedDate).setHours(17, 0, 0, 0)}
+            />
+          </>
+        )}
       </div>
     </div>
   );
